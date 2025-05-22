b700e94a96e41ab1f675ad4356ab0cd9503c0c7e
kenley <gaitan19>
2025-05-22 11:26:38 -0600

    Refactor code structure for improved readability and maintainability

--- EventsManagementsApp.Test/SponsorControllerTests.cs
+++ EventsManagementsApp.Test/SponsorControllerTests.cs
@@ -1,61 +1,72 @@
 ﻿using EventsManagementApp.Controllers;
 using EventsManagementApp.DTOs;
 using EventsManagementApp.Models;
+using EventsManagementApp.Repositories;
 using EventsManagementApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace EventsManagementApp.Tests.Controllers
 {
     public class SponsorControllerTests
     {
-        private readonly Mock<SponsorService> _mockService;
+        private readonly Mock<ISponsorRepository> _mockRepository;
+        private readonly SponsorService _sponsorService;
         private readonly SponsorController _controller;
 
         public SponsorControllerTests()
         {
-            _mockService = new Mock<SponsorService>();
-            _controller = new SponsorController(_mockService.Object);
+            _mockRepository = new Mock<ISponsorRepository>();
+            _sponsorService = new SponsorService(_mockRepository.Object);
+            _controller = new SponsorController(_sponsorService);
         }
 
         [Fact]
-        public void GetAll_ReturnsOkWithSponsors()
+        public void GetAll_ReturnsOkResultWithSponsors()
         {
             // Arrange  
             var sponsors = new List<Sponsor>
             {
-                new Sponsor { Id = Guid.NewGuid(), Name = "Sponsor 1" },
-                new Sponsor { Id = Guid.NewGuid(), Name = "Sponsor 2" }
+                new Sponsor { Id = Guid.NewGuid(), Name = "Sponsor 1", Description = "Description 1", EventId = Guid.NewGuid() },
+                new Sponsor { Id = Guid.NewGuid(), Name = "Sponsor 2", Description = "Description 2", EventId = Guid.NewGuid() }
             };
-            _mockService.Setup(s => s.GetAll()).Returns(sponsors);
+            _mockRepository.Setup(repo => repo.GetAll()).Returns(sponsors);
 
             // Act  
             var result = _controller.GetAll();
 
             // Assert  
             var okResult = Assert.IsType<OkObjectResult>(result.Result);
-            Assert.Equal(sponsors, okResult.Value);
-            _mockService.Verify(s => s.GetAll(), Times.Once);
+            var returnedSponsors = Assert.IsAssignableFrom<IEnumerable<Sponsor>>(okResult.Value);
+            Assert.Equal(2, returnedSponsors.Count());
         }
 
         [Fact]
-        public void GetById_ExistingId_ReturnsOkWithSponsor()
+        public void GetById_ExistingId_ReturnsOkResultWithSponsor()
         {
             // Arrange  
             var sponsorId = Guid.NewGuid();
-            var sponsor = new Sponsor { Id = sponsorId, Name = "Test Sponsor" };
-            _mockService.Setup(s => s.GetById(sponsorId)).Returns(sponsor);
+            var sponsor = new Sponsor
+            {
+                Id = sponsorId,
+                Name = "Test Sponsor",
+                Description = "Test Description",
+                EventId = Guid.NewGuid()
+            };
+            _mockRepository.Setup(repo => repo.GetById(sponsorId)).Returns(sponsor);
 
             // Act  
             var result = _controller.GetById(sponsorId);
 
             // Assert  
             var okResult = Assert.IsType<OkObjectResult>(result.Result);
-            Assert.Equal(sponsor, okResult.Value);
-            _mockService.Verify(s => s.GetById(sponsorId), Times.Once);
+            var returnedSponsor = Assert.IsType<Sponsor>(okResult.Value);
+            Assert.Equal(sponsorId, returnedSponsor.Id);
+            Assert.Equal("Test Sponsor", returnedSponsor.Name);
         }
 
         [Fact]
@@ -63,21 +74,27 @@ namespace EventsManagementApp.Tests.Controllers
         {
             // Arrange  
             var sponsorId = Guid.NewGuid();
-            _mockService.Setup(s => s.GetById(sponsorId)).Returns((Sponsor)null);
+            _mockRepository.Setup(repo => repo.GetById(sponsorId)).Returns((Sponsor)null);
 
             // Act  
             var result = _controller.GetById(sponsorId);
 
             // Assert  
             Assert.IsType<NotFoundResult>(result.Result);
-            _mockService.Verify(s => s.GetById(sponsorId), Times.Once);
         }
 
         [Fact]
-        public void Add_ValidSponsor_ReturnsCreatedAtAction()
+        public void Add_ValidSponsorDto_ReturnsCreatedAtActionResult()
         {
             // Arrange  
-            var sponsorDto = new SponsorDTO { Name = "New Sponsor" };
+            var sponsorDto = new SponsorDTO
+            {
+                Name = "New Sponsor",
+                Description = "New Description",
+                EventId = Guid.NewGuid()
+            };
+            _mockRepository.Setup(repo => repo.Add(It.IsAny<Sponsor>()));
+            _mockRepository.Setup(repo => repo.SaveChanges());
 
             // Act  
             var result = _controller.Add(sponsorDto);
@@ -85,36 +102,82 @@ namespace EventsManagementApp.Tests.Controllers
             // Assert  
             var createdResult = Assert.IsType<CreatedAtActionResult>(result);
             Assert.Equal(nameof(_controller.GetAll), createdResult.ActionName);
-            _mockService.Verify(s => s.Add(sponsorDto), Times.Once);
+            _mockRepository.Verify(repo => repo.Add(It.IsAny<Sponsor>()), Times.Once);
+            _mockRepository.Verify(repo => repo.SaveChanges(), Times.Once);
         }
 
         [Fact]
-        public void Update_ValidSponsor_ReturnsNoContent()
+        public void Update_ExistingId_ReturnsNoContent()
         {
             // Arrange  
             var sponsorId = Guid.NewGuid();
-            var sponsorDto = new SponsorDTO { Name = "Updated Sponsor" };
+            var existingSponsor = new Sponsor
+            {
+                Id = sponsorId,
+                Name = "Original Name",
+                Description = "Original Description",
+                EventId = Guid.NewGuid()
+            };
+            var sponsorDto = new SponsorDTO
+            {
+                Name = "Updated Name",
+                Description = "Updated Description",
+                EventId = Guid.NewGuid()
+            };
+
+            _mockRepository.Setup(repo => repo.GetById(sponsorId)).Returns(existingSponsor);
+            _mockRepository.Setup(repo => repo.Update(It.IsAny<Sponsor>()));
+            _mockRepository.Setup(repo => repo.SaveChanges());
 
             // Act  
             var result = _controller.Update(sponsorId, sponsorDto);
 
             // Assert  
             Assert.IsType<NoContentResult>(result);
-            _mockService.Verify(s => s.Update(sponsorId, sponsorDto), Times.Once);
+            _mockRepository.Verify(repo => repo.GetById(sponsorId), Times.Once);
+            _mockRepository.Verify(repo => repo.Update(It.IsAny<Sponsor>()), Times.Once);
+            _mockRepository.Verify(repo => repo.SaveChanges(), Times.Once);
         }
 
         [Fact]
-        public void Delete_ValidId_ReturnsNoContent()
+        public void Update_NonExistingId_CallsRepositoryButDoesNotUpdate()
         {
             // Arrange  
             var sponsorId = Guid.NewGuid();
+            var sponsorDto = new SponsorDTO
+            {
+                Name = "Updated Name",
+                Description = "Updated Description",
+                EventId = Guid.NewGuid()
+            };
+
+            _mockRepository.Setup(repo => repo.GetById(sponsorId)).Returns((Sponsor)null);
+
+            // Act  
+            var result = _controller.Update(sponsorId, sponsorDto);
+
+            // Assert  
+            Assert.IsType<NoContentResult>(result);
+            _mockRepository.Verify(repo => repo.GetById(sponsorId), Times.Once);
+            _mockRepository.Verify(repo => repo.Update(It.IsAny<Sponsor>()), Times.Never);
+            _mockRepository.Verify(repo => repo.SaveChanges(), Times.Never);
+        }
+
+        [Fact]
+        public void Delete_ExistingId_ReturnsNoContent()
+        {
+            // Arrange  
+            var sponsorId = Guid.NewGuid();
+            _mockRepository.Setup(repo => repo.Delete(sponsorId));
+            _mockRepository.Setup(repo => repo.SaveChanges());
 
             // Act  
             var result = _controller.Delete(sponsorId);
 
             // Assert  
             Assert.IsType<NoContentResult>(result);
-            _mockService.Verify(s => s.Delete(sponsorId), Times.Once);
+            _mockRepository.Verify(repo => repo.Delete(sponsorId), Times.Once);
+            _mockRepository.Verify(repo => repo.SaveChanges(), Times.Once);
         }
     }
 }
